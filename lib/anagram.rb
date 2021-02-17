ce0df4cc68486eae77394cb6895771257b05b67f
jgumerove <josh.gumerove@gmail.com>
2021-02-17 18:26:29 -0500

    commit tree 72d754f004a455363a6e0d5be24044477076bf2f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  def initialize(letters)
+    @letters = letters
+  end
+
+  def match(words)
+    words.select do |word|
+      @letters.chars.sort == word.chars.sort
+      #is_anagram?(word)
+    end
+  end
+
+  #def is_anagram?(word)
+    #word.chars.sort == @letters.chars.sort #not that you can pass a method into a method like above
+  #end
+end
+#very important functionality method above 
